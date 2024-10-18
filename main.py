f7c64838725e47b3bffd86dfd09d1897358a5d30
Hadesiak130-alt <agnieszkafraczkowska89+github@gmail.com>
2024-10-18 17:39:04 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+m2km = 0.001
+km2m = 1000
+cm2dm = 0.1
+dm2cm = 10
+mm2cm = 0.1
+cm2mm = 10
+dm2m = 0.1
+m2dm = 10
+mm2m = 0.001
+m2mm = 1000
+mm2km = 0.000001
+km2mm = 1000000
+mm2dm = 0.1
+dm2mm = 100
+cm2m = 0.01
+m2cm = 100
+
